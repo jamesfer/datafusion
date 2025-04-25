826752aefd2cac08a6c13653b565eee32579eb50
James Ferguson <jamesferguson497@gmail.com>
2025-04-25 22:05:18 +0200

    Improve projection

--- datafusion/physical-plan/src/streaming_operators/projection.rs
+++ datafusion/physical-plan/src/streaming_operators/projection.rs
@@ -1,10 +1,9 @@
-use std::sync::Arc;
 use arrow::array::RecordBatchOptions;
 use arrow::record_batch::RecordBatch;
-use arrow_schema::SchemaRef;
+use arrow_schema::{Field, Schema, SchemaRef};
 use datafusion_common::DataFusionError;
 use datafusion_physical_expr_common::physical_expr::PhysicalExpr;
-use crate::projection::ProjectionExec;
+use std::sync::Arc;
 
 // impl TryInto<ProjectionStreamingTask> for ProjectionExec {
 //     type Error = ();
@@ -24,13 +23,24 @@ use crate::projection::ProjectionExec;
 // }
 
 pub struct ProjectionStreamingTask {
-    schema: SchemaRef,
+    output_schema: SchemaRef,
     expr: Vec<Arc<dyn PhysicalExpr>>,
 }
 
 impl ProjectionStreamingTask {
-    pub fn new(schema: SchemaRef, expr: Vec<Arc<dyn PhysicalExpr>>) -> Self {
-        Self { schema, expr }
+    pub fn new(expressions: Vec<(Arc<dyn PhysicalExpr>, String)>, input_schema: SchemaRef) -> Result<Self, DataFusionError> {
+        let fields = expressions.iter()
+            .map(|(expr, name)| Ok::<_, DataFusionError>(Field::new(
+                name,
+                expr.data_type(&input_schema)?,
+                expr.nullable(&input_schema)?,
+            )))
+            .collect::<Result<Vec<_>, _>>()?;
+        let output_schema = Schema::new_with_metadata(fields, input_schema.metadata().clone());
+        Ok(Self {
+            output_schema: SchemaRef::new(output_schema),
+            expr: expressions.into_iter().map(|(expr, _name)| expr).collect()
+        })
     }
 }
 
@@ -50,10 +60,10 @@ impl ProjectionStreamingTask {
         if arrays.is_empty() {
             let options =
                 RecordBatchOptions::new().with_row_count(Some(batch.num_rows()));
-            RecordBatch::try_new_with_options(Arc::clone(&self.schema), arrays, &options)
+            RecordBatch::try_new_with_options(Arc::clone(&self.output_schema), arrays, &options)
                 .map_err(Into::into)
         } else {
-            RecordBatch::try_new(Arc::clone(&self.schema), arrays).map_err(Into::into)
+            RecordBatch::try_new(Arc::clone(&self.output_schema), arrays).map_err(Into::into)
         }
     }
 }
